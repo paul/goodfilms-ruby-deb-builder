02a17a55bed2f639452c389d723b43759903f2bd
Paul Sadauskas <psadauskas@gmail.com>
2014-05-06 12:49:26 -0700

    Build using sam's discourse patches

--- package-ruby.sh
+++ package-ruby.sh
@@ -18,11 +18,13 @@ sudo gem install fpm
 fi
 
 RUBY_VERSION="2.1.1"
-SOURCE_FILE_NAME=ruby-github-${RUBY_VERSION}
+RUBY_NAME=ruby-discourse
+SOURCE_FILE_NAME=${RUBY_NAME}-${RUBY_VERSION}
 INSTALL_DIR=/tmp/install
 WORKING_DIRECTORY=/tmp/build
 PACKAGE_NAME=${SOURCE_FILE_NAME}_amd64.deb
-GITHUB_REPO=https://github.com/github/ruby.git
+#GITHUB_REPO=https://github.com/github/ruby.git
+GITHUB_REPO=https://github.com/SamSaffron/ruby.git
 
 # clean any previous build artifacts
 rm -rf ${INSTALL_DIR}
@@ -47,12 +49,12 @@ else
   cd ruby
 fi
 
-time (./configure --prefix=/usr/local --with-opt-dir=/usr/local && make && make install DESTDIR=${INSTALL_DIR})
+time (autoconf && ./configure --prefix=/usr/local --with-opt-dir=/usr/local && make && make install DESTDIR=${INSTALL_DIR})
 cd ..
 
 # package up the newly compiled ruby
-fpm -s dir -t deb -n ruby-github -v ${RUBY_VERSION} -C ${INSTALL_DIR} \
-  -p ruby-github-VERSION_ARCH.deb -d "libstdc++6 (>= 4.4.3)" \
+fpm -s dir -t deb -n ${RUBY_NAME} -v ${RUBY_VERSION} -C ${INSTALL_DIR} \
+  -p ${RUBY_NAME}-VERSION_ARCH.deb -d "libstdc++6 (>= 4.4.3)" \
   -d "libc6 (>= 2.6)" -d "libffi6 (>= 3.0.10)" -d "libgdbm3 (>= 1.8.3)" \
   -d "libncurses5 (>= 5.7)" -d "libreadline6 (>= 6.1)" \
   -d "libssl1.0.0 (>= 1.0.0)" -d "zlib1g (>= 1:1.2.2)" \
